0b7a6a9254a39064a00cc052ef37d832b7d47f1b
Giovanni Izzi <giovanniizzi92@hotmail.it>
2017-11-26 09:50:31 +0100

    Added communication with Dexter-webapp.

--- src/main/java/swap/gerbilannotatorsample/GerbilAnnotator.java
+++ src/main/java/swap/gerbilannotatorsample/GerbilAnnotator.java
@@ -6,10 +6,15 @@
 package swap.gerbilannotatorsample;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.Reader;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.aksw.gerbil.transfer.nif.Document;
@@ -18,9 +23,18 @@ import org.aksw.gerbil.transfer.nif.Span;
 import org.aksw.gerbil.transfer.nif.TurtleNIFDocumentCreator;
 import org.aksw.gerbil.transfer.nif.TurtleNIFDocumentParser;
 import org.aksw.gerbil.transfer.nif.data.NamedEntity;
+import org.aksw.gerbil.transfer.nif.data.SpanImpl;
 import org.restlet.representation.Representation;
 import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
+
+import com.google.gson.GsonBuilder;
+
+import it.cnr.isti.hpc.dexter.common.Field;
+import it.cnr.isti.hpc.dexter.common.MultifieldDocument;
+import it.cnr.isti.hpc.dexter.rest.client.DexterRestClient;
+import it.cnr.isti.hpc.dexter.rest.domain.AnnotatedDocument;
+import it.cnr.isti.hpc.dexter.rest.domain.AnnotatedSpot;
 
 /**
  *
@@ -33,6 +47,7 @@ public class GerbilAnnotator extends ServerResource {
     private final TurtleNIFDocumentParser parser = new TurtleNIFDocumentParser();
 
     private final TurtleNIFDocumentCreator creator = new TurtleNIFDocumentCreator();
+    
 
     @Post
     public String accept(Representation request) {
@@ -51,13 +66,110 @@ public class GerbilAnnotator extends ServerResource {
             return "";
         }
         String text = document.getText();
+        System.out.println(text + "\n");
+        
+        
+        List<Marking> mark = document.getMarkings();
+        String s = "";
+        for(Iterator<Marking> it = mark.iterator(); it.hasNext(); )
+        {
+        	Marking temp = it.next();
+        	SpanImpl n = (SpanImpl) temp;
+        	int start = n.getStartPosition();
+        	int len = n.getLength();
+        	String mention = text.substring(start, (start + len)).toLowerCase();
+        	String add = mention + ":" + start + "," + (start + len) + ";";
+        	
+        	/*
+        	if(add.equalsIgnoreCase(mention+":"+";"))
+        	{
+        		Scanner in = new Scanner(System.in);
+        		String userInput = in.nextLine();
+        		in.close();
+        	}
+        	 */
+        	s = s + add;
+        }
+        
+        System.out.println(s);
+        System.out.println("Fine menzioni Gerbil");
+        
+        /*
+        try 
+        {
+        	FileOutputStream prova = new FileOutputStream("/Users/giovanniizzi/Desktop/dexter.txt", true);
+            PrintStream scrivi = new PrintStream(prova);
+            scrivi.println(text);
+            scrivi.println();
+            scrivi.println(s);
+            scrivi.println();
+            scrivi.println();
+            scrivi.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println("Errore: " + e);
+        }
+        */
+        
+        if (s == "")
+        {
+        	String nifDocument = creator.getDocumentAsNIFString(document);
+            return nifDocument;
+        }
+        
+        
+        
+        DexterRestClient client;
+        try 
+        {
+			client = new DexterRestClient("http://localhost:8080/dexter-webapp/api/rest");
+		} 
+        catch (URISyntaxException e) 
+        {
+			e.printStackTrace();
+			return null;
+		}
+        
+        MultifieldDocument multiDocument = new MultifieldDocument();
+        multiDocument.addField(new Field("body", s));
+        
+        client.setSpotter("nif");
+        client.setDisambiguator("genetic");
+		
+		client.setLinkProbability(0.03);
+		client.setWikinames(true);
+
+		AnnotatedDocument sd = client.annotate(multiDocument);
+		System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(sd));
+		System.out.println("Fine stampa gsonBuilder");
+		
+		List<AnnotatedSpot> annSpot = sd.getSpots();
+		
+        
         //TO DO annotate text
-        List<Span> esl = document.getMarkings(Span.class);
+        //List<Span> esl = document.getMarkings(Span.class);
+		
         // Annotator should return marking
         List<Marking> entities=new ArrayList<>();
+		
         // example of marking (start, length, uri)
-        entities.add(new NamedEntity(43, 10, "uri"));
+        //entities.add(new NamedEntity(43, 10, "uri"));
+        
+        String baseUri = "http://dbpedia.org/resource/";
+        //String baseUri = "http://en.wikipedia.org/wiki/";
+        for(Iterator<AnnotatedSpot> it = annSpot.iterator(); it.hasNext(); )
+        {
+        	AnnotatedSpot as = it.next();
+        	String uri = baseUri + as.getWikiname();
+        	int len = as.getEnd() - as.getStart();
+        	entities.add(new NamedEntity(as.getStart(), len, uri));
+        	
+        }
+        
         document.setMarkings(entities);
+        
+        
         //return document
         String nifDocument = creator.getDocumentAsNIFString(document);
         return nifDocument;
--- src/main/java/swap/gerbilannotatorsample/GerbilService.java
+++ src/main/java/swap/gerbilannotatorsample/GerbilService.java
@@ -5,10 +5,23 @@
  */
 package swap.gerbilannotatorsample;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.Inet4Address;
+import java.net.Socket;
+import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.restlet.Component;
 import org.restlet.data.Protocol;
+
+import info.debatty.java.stringsimilarity.JaroWinkler;
+import info.debatty.java.stringsimilarity.NormalizedLevenshtein;
 
 /**
  *
@@ -19,14 +32,40 @@ public class GerbilService {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args) 
+    {
+        try 
+        {
+        	System.out.println(Inet4Address.getLocalHost().getHostAddress());
             // Create a new Component.
             Component component = new Component();
-
+            
+           
+            if(args.length > 1)
+            {
+            	
+            
             // Add a new HTTP server listening.
-            component.getServers().add(Protocol.HTTP, args[0], Integer.parseInt(args[1]));
-
+            	component.getServers().add(Protocol.HTTP, args[0], Integer.parseInt(args[1]));
+            }
+            else
+            	component.getServers().add(Protocol.HTTP, "localhost", 8980);
+            
+            //Test server prof
+            
+            //GET
+            /*
+            URL url = new URL("http://193.204.187.35:9100/nesim/sim/5981816/77877");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            BufferedReader read = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+            String line = read.readLine();
+            System.out.println(line);
+            read.close();
+            */
+         
+			
+            
+            
             // Attach the sample application.
             component.getDefaultHost().attach("/gerbil", new GerbilApplication());
 
